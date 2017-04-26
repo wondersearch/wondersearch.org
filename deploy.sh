89f9937befbc20396317f12f2f06e8b4c609c321
Melvin Carvalho <melvincarvalho@gmail.com>
2017-04-26 22:23:48 +0200

    Add deploy script and gh pages

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git update-index --assume-unchanged bin/dbconfig.js
+
+git add -u
+git commit -m "$1"
+git push origin master
+
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+
+git checkout master
+
